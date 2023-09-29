649ecfd95b394773960df928656f7a016bc11dd7
AwSumYin <saww0004@student.monash.edu>
2023-09-29 12:06:39 +0800

    update error handling for duplicate task/sprint

--- src/scripts/create-task-script.js
+++ src/scripts/create-task-script.js
@@ -1,14 +1,15 @@
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-app.js";
-import { getDatabase, ref, set, get, child, push} from "https://www.gstatic.com/firebasejs/10.3.1/firebase-database.js";
+import { getDatabase, ref, set, get, child, push } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-database.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyACyBE4-v3Z5qL37njca-CaPUPXMHfzZbY",
-    authDomain: "fit2101-team4.firebaseapp.com",
-    databaseURL: "https://fit2101-team4-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "fit2101-team4",
-    storageBucket: "fit2101-team4.appspot.com",
-    messagingSenderId: "413420787548",
-    appId: "1:413420787548:web:be8873c231ec4468845399"
+  apiKey: "AIzaSyACyBE4-v3Z5qL37njca-CaPUPXMHfzZbY",
+  authDomain: "fit2101-team4.firebaseapp.com",
+  databaseURL: "https://fit2101-team4-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "fit2101-team4",
+  storageBucket: "fit2101-team4.appspot.com",
+  messagingSenderId: "413420787548",
+  appId: "1:413420787548:web:be8873c231ec4468845399"
 };
 
 // Initialize Firebase
@@ -17,7 +18,6 @@ const db = getDatabase(app);
 
 // Event Listener
 document.getElementById("create-task-btn").addEventListener('click', (e) => {
-
   let cb = document.querySelectorAll('#task-tag input[type="checkbox"]');
   let tags = [];
   cb.forEach((b) => {
@@ -25,54 +25,72 @@ document.getElementById("create-task-btn").addEventListener('click', (e) => {
   })
   console.log(tags)
 
-  const name = document.getElementById("task-name").value
-  const story_point = document.getElementById("task-story-point").value
-  const assignee = document.getElementById("task-assignee").value
-  const description = document.getElementById("task-description").value
-  const type = document.getElementById("task-type").value
+  const name = document.getElementById("task-name").value;
+  const story_point = document.getElementById("task-story-point").value;
+  const assignee = document.getElementById("task-assignee").value;
+  const description = document.getElementById("task-description").value;
+  const type = document.getElementById("task-type").value;
   const tag = JSON.stringify(tags);
-  const priority = document.getElementById("task-priority").value
-  const status = document.getElementById("task-status").value
-  const stages = document.getElementById("task-stages").value
-  const date = new Date().toString()
-  
+  const priority = document.getElementById("task-priority").value;
+  const status = document.getElementById("task-status").value;
+  const stages = document.getElementById("task-stages").value;
+  const date = new Date().toString();
+
   e.preventDefault();
 
-  set(ref(db, "task/" + name),{
-    name: name,
-    story_point: story_point,
-    assignee: assignee,
-    description: description,
-    type: type,
-    tag: tag,
-    priority: priority,
-    status: status,
-    stages: stages,
-    date: date,
-    logtime: JSON.stringify({})
-  })
-  .then(
-    () => {alert("Task Created!")}
-  )
-  .catch((error) => {alert(error)})
-})
+  // Check if the task name already exists
+  const taskRef = ref(db, "task/" + name);
+  get(taskRef)
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        // Task name already exists, show an error message
+        alert("Task with the same name already exists!");
+      } else {
+        // Task name is unique, proceed with task creation
+        set(ref(db, "task/" + name), {
+          name: name,
+          story_point: story_point,
+          assignee: assignee,
+          description: description,
+          type: type,
+          tag: tag,
+          priority: priority,
+          status: status,
+          stages: stages,
+          date: date,
+          logtime: JSON.stringify({})
+        })
+          .then(() => {
+            alert("Task Created!");
+          })
+          .catch((error) => {
+            alert(error);
+          });
+      }
+    })
+    .catch((error) => {
+      alert(error);
+    });
+});
 
-const priority = document.getElementById("task-priority")
-priority.addEventListener("change", function() {
-  priority.style.backgroundColor = getPriorityColor(priority.value)
-})
+const priority = document.getElementById("task-priority");
+priority.addEventListener("change", function () {
+  priority.style.backgroundColor = getPriorityColor(priority.value);
+});
 
-document.getElementById("return-product-backlog-btn").addEventListener('click', () => {window.open('product-backlog.html', "_self")})
+document.getElementById("return-product-backlog-btn").addEventListener('click', () => {
+  window.open('product-backlog.html', "_self");
+});
 
 function getPriorityColor(priority) {
   switch (priority) {
     case "Urgent":
-      return "#F65B51"
+      return "#F65B51";
     case "Important":
-      return "lightsalmon"
+      return "lightsalmon";
     case "Medium":
-      return "#FFFA84"
+      return "#FFFA84";
     case "Low":
-      return "lightgreen"
+      return "lightgreen";
   }
 }

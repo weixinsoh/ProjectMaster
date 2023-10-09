3e2f0903c4a2de08e9218e274e07a96b8c6cad04
phoebetingweijia <ptin0004@student.monash.edu>
2023-10-10 01:32:56 +0800

    US3 - ST2: frontend functionality for logout page

--- src/scripts/admin-view-script.js
+++ src/scripts/admin-view-script.js
@@ -13,3 +13,17 @@ card.innerHTML = `
 </p>
 `
 document.getElementById("Username").appendChild(card);
+
+//  Add a confirmation dialog to ensure users want to log out before proceeding
+function confirmLogout() {
+  const confirmDialog = confirm("Are you sure you want to log out?");
+  if (confirmDialog) {
+    // If the user confirms, proceed with the logout
+    window.location.href = "login-page.html"; // Redirect to the login page
+  }
+}
+
+document.getElementById("logout-link").addEventListener("click", function (event) {
+  event.preventDefault(); // Prevent the default link behavior
+  confirmLogout(); // Show the confirmation dialog
+});
--- src/scripts/scrum-board-script.js
+++ src/scripts/scrum-board-script.js
@@ -209,3 +209,17 @@ async function calcTotalSP(tasks) {
 }
 
 initialStoryPointUpdate()
+
+//  Add a confirmation dialog to ensure users want to log out before proceeding
+function confirmLogout() {
+  const confirmDialog = confirm("Are you sure you want to log out?");
+  if (confirmDialog) {
+    // If the user confirms, proceed with the logout
+    window.location.href = "login-page.html"; // Redirect to the login page
+  }
+}
+
+document.getElementById("logout-link").addEventListener("click", function (event) {
+  event.preventDefault(); // Prevent the default link behavior
+  confirmLogout(); // Show the confirmation dialog
+});

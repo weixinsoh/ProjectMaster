c86bb258a3a35312d43cc70793313085282feb2e
jwon0147 <jwon0147@student.monash.edu>
2023-10-15 13:21:24 +0800

    US:ST4 modified backend for theme update

--- src/scripts/theme-update.js
+++ src/scripts/theme-update.js
@@ -1,5 +1,23 @@
-import { toggleTheme } from "./util.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-app.js";
+import { getDatabase, ref, get, onValue, update} from "https://www.gstatic.com/firebasejs/10.3.1/firebase-database.js";
+import { toggleTheme } from "./util.js";
 
-console.log(localStorage.getItem("theme"))
-const theme = localStorage.getItem("theme")
-theme == null ? toggleTheme("theme-default") : toggleTheme(theme)
+const firebaseConfig = {
+    apiKey: "AIzaSyACyBE4-v3Z5qL37njca-CaPUPXMHfzZbY",
+    authDomain: "fit2101-team4.firebaseapp.com",
+    databaseURL: "https://fit2101-team4-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "fit2101-team4",
+    storageBucket: "fit2101-team4.appspot.com",
+    messagingSenderId: "413420787548",
+    appId: "1:413420787548:web:be8873c231ec4468845399"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+const username = localStorage.getItem('username')
+
+get(ref(db, "users/" + username)).then((snapshot) => {
+    const data = snapshot.val()
+    toggleTheme(data.theme)
+})
